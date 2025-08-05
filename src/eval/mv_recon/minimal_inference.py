293d268a8737eb44b72c271cc09f678d5c8db079
lyttttt3333 <135998170+lyttttt3333@users.noreply.github.com>
2025-08-05 23:43:00 +0800

    Update minimal_inference.py

--- src/eval/mv_recon/minimal_inference.py
+++ src/eval/mv_recon/minimal_inference.py
@@ -18,7 +18,7 @@ import uuid
 import json
 from collections import defaultdict
 
-def create_fake_frames(num_frames=4, img_channels=3, img_height=256, img_width=256):
+def create_fake_frames(num_frames=4, img_channels=3, img_height=512, img_width=384):
     images = torch.zeros((num_frames, img_channels, img_height, img_width), dtype=torch.float32)
     
     frames = []
