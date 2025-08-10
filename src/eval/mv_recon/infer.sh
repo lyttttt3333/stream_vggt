a1b23f01c8c5dbb3e5ef0114006725b21e5cc567
lyttttt3333 <135998170+lyttttt3333@users.noreply.github.com>
2025-08-10 12:59:58 +0800

    .

--- src/eval/mv_recon/infer.sh
+++ src/eval/mv_recon/infer.sh
@@ -9,7 +9,7 @@ model_weights="${workdir}/ckpt/${ckpt_name}.pth"
 
 output_dir="${workdir}/eval_results/mv_recon/${model_name}_${ckpt_name}"
 echo "$output_dir"
-accelerate launch --num_processes 1 --main_process_port 29602 ./eval/mv_recon/minimal_inference.py \
+accelerate launch --num_processes 1 --main_process_port 29602 src/eval/mv_recon/minimal_inference.py \
     --weights "$model_weights" \
     --output_dir "$output_dir" \
     --model_name "$model_name" \
